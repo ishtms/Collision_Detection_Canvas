38a598854544f3d1ec7ecf0e53ddc9364f73f779
Ishtmeet Singh <ish.rissam@gmail.com>
2018-03-29 16:56:48 +0530

    added performance fix, when ball wasn't able to move

--- Ball.js
+++ Ball.js
@@ -6,13 +6,13 @@ var Ball = function(_xPos, _yPos, _radius, _speed, _colour) {
     this.speed = _speed;
     this.colour = _colour;
 
-    this.draw = function() {
+    this.drawBall = function() {
         fill(this.colour);
         ellipse(this.xPos, this.yPos, this.radius * 2, this.radius * 2);
     }
 
     this.update = function() {
-        this.draw();
+        this.drawBall();
         console.log('update')
         if (this.xPos < this.radius || this.yPos > width - this.radius) {
             this.speed *= -1;
