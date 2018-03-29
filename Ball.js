89ad2752499c53c3f70c14ad92bc0629ed5dcc06
Ishtmeet Singh <ish.rissam@gmail.com>
2018-03-29 18:53:43 +0530

    changed fix for balls stuck when generated below width - radius

--- Ball.js
+++ Ball.js
@@ -1,28 +1,51 @@
-var Ball = function(_xPos, _yPos, _radius, _speed, _colour) {
+function calculateXPosition(_pos, _radius) {
+    if (_pos < _radius) {
+        return _pos + _radius;
+    } else if (_pos > width - _radius) {
+        return width - _pos;
+    } else return _pos
+}
 
-    this.xPos = _xPos;
-    this.yPos = _yPos;
+function calculateYPosition(_pos, _radius) {
+    if (_pos < _radius) {
+        return _pos + _radius;
+    } else if (_pos > height - _radius) {
+        return height - _pos;
+    } else return _pos
+}
+
+var Ball = function(_xPos, _yPos, _radius, _speedX, _speedY, _colour) {
+    var xValue =
+    this.xPos = calculateXPosition(_xPos, _radius);
+    this.yPos = calculateYPosition(_yPos, _radius);
     this.radius = _radius;
-    this.speed = _speed;
+    this.speedX = floor(_speedX) === 0 ? _speedX + 1 : _speedX;
+    this.speedY = floor(_speedY) === 0 ? _speedY + 1 : _speedY;
     this.colour = _colour;
 
     this.drawBall = function() {
         fill(this.colour);
-        ellipse(this.xPos, this.yPos, this.radius * 2, this.radius * 2);
+        ellipse(this.xPos + this.radius, this.yPos + this.radius, this.radius * 2, this.radius * 2);
     }
 
     this.update = function() {
         this.drawBall();
-        console.log('update')
-        if (this.xPos < this.radius || this.yPos > width - this.radius) {
-            this.speed *= -1;
+        if (this.xPos < 0 || this.xPos > width - this.radius) {
+            this.speedX *= -1;
         }
 
-        this.xPos += this.speed;
+        if (this.yPos < 0 || this.yPos > height - this.radius) {
+            this.speedY *= -1;
+        }
+
+        this.xPos += this.speedX;
+        this.yPos += this.speedY;
     }
 
-    this.checkDetection = function(_ball) {
-        
+    this.checkPressed = function(_xPos, _yPos) {
+        if (dist(this.xPos, this.yPos, _xPos, _yPos) < this.radius * 2) {
+            this.xPos
+        }
     }
 
 }
