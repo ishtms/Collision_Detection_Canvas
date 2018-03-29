c08c23029cbdb8da3a01f6494f96dc99865e63bf
Ishtmeet Singh <ish.rissam@gmail.com>
2018-03-29 16:52:35 +0530

    first change

---
+++ Ball.js
@@ -0,0 +1,28 @@
+var Ball = function(_xPos, _yPos, _radius, _speed, _colour) {
+
+    this.xPos = _xPos;
+    this.yPos = _yPos;
+    this.radius = _radius;
+    this.speed = _speed;
+    this.colour = _colour;
+
+    this.draw = function() {
+        fill(this.colour);
+        ellipse(this.xPos, this.yPos, this.radius * 2, this.radius * 2);
+    }
+
+    this.update = function() {
+        this.draw();
+        console.log('update')
+        if (this.xPos < this.radius || this.yPos > width - this.radius) {
+            this.speed *= -1;
+        }
+
+        this.xPos += this.speed;
+    }
+
+    this.checkDetection = function(_ball) {
+        
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+var ballContainer = [];
+
+function randomXYGenerator(_limit) {
+    return floor(random(0, _limit));
+}
+
+function randomRadiusGenerator() {
+    return random(20, 80);
+}
+
+function randomSpeedGenerator() {
+    return random(-10, 10);
+}
+
+function randomColorGenerator() {
+    return color(random(0, 255), random(0, 255), random(0, 255));
+}
+
+function drawBalls() {
+    console.log(ballContainer.length)
+    for(var index = 0; index < ballContainer.length; index++) {
+        ballContainer[index].update();
+    }
+}
+
 function setup() {
-  
+    createCanvas(800);
+    for (var index = 0; index < 20; index++) {
+        ballContainer.push(
+            new Ball(
+                randomXYGenerator(width),
+                randomXYGenerator(height),
+                randomRadiusGenerator(),
+                randomSpeedGenerator(),
+                randomColorGenerator()
+            )
+        );
+    }
 }
 
 function draw() {
-  
+    background(123)
+    drawBalls();
 }
+
